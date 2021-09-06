bea3441947231802b85305548fc34e8726b7eece
Riaz <81258483+R14Z@users.noreply.github.com>
2021-09-06 18:33:38 +0100

    Add files via upload

---
+++ mad libs game.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+flower = input("Enter a flower: ")
+hobby = input("Enter a hobby: ")
+
+print(f'Roses are {color}. {flower} are blue and one of my hobbies are {hobby}.')
+
+
